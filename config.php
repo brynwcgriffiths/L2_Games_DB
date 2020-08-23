265df52db16276b3609ca734850d02bde1d8e6d9
brynwcgriffiths <66233176+brynwcgriffiths@users.noreply.github.com>
2020-08-24 09:53:26 +1200

    Changed define

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 'On'); //Change to off when live
 
 define("DB_NAME","00_l2_games");
 define("DB_USERNAME","root");
-define("DB_PASSWORD","");
+define("DB_PASSWORD","usbw");
 define("DB_HOST","localhost");
 
 ?>
--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 				<h2>Welcome</h2>
 				
 				<p>
-					Put link to original dataset here
+					The database includes some of the most popular apps avaliable
+					for mobile devices. The <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games">
+					original dataset</a> can be found at the proceeding link.
 				</p>
 			</div> <!-- / main -->
 			
