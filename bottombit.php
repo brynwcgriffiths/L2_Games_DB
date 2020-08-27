b625d139dd28f531d221877dc8e5dc26ae828136
brynwcgriffiths <66233176+brynwcgriffiths@users.noreply.github.com>
2020-08-27 14:47:37 +1200

    Basic form

--- bottombit.php
+++ bottombit.php
@@ -2,7 +2,7 @@
 				<h2>Add an App |
                     <a class ="side"href="showall.php"> Show All</a></h2>
 				<form class = "searchform" method="post" action="name_dev.php" enctype="multipart/form-data">
-
+					<input class="search" type="text" name="dev_name" size="40" value="" required placeholder="App Name / Developer Name..." />
 				</form>
 			</div> <!-- / side -->
 
